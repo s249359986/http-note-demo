cb9c0c9707b6302adfeb0a01d8f064263852d242
songdonghong <songdonghong@daojiao-inc.com>
2018-11-06 09:58:59 +0800

    修改实例

--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,31 @@ var fs = require('fs');
 var router = express.Router();
 var path = require('path');
 /* GET home page. */
+
+router.get('/noCach', function(req, res, next) {
+  res.render('index',{title:'aa'})
+});
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '约谈http' });
+  res.sendFile(path.join(__dirname,'../public/html/cache.html'),{maxAge:'3600000'})
 });
 router.get('/cache', function(req, res, next) {
   res.sendFile(path.join(__dirname,'../public/html/cache.html'),{maxAge:'3600000'})
 });
 
-
 router.get('/style.css', function(req, res, next) {
   const cssPath = path.join(__dirname,'../public/stylesheets/style.css')
+  console.log('style.css')
   fs.readFile(cssPath,(error,content) => {
-    res.setHeader('Cache-Control','public,max-age=20')
+    res.setHeader('Cache-Control','max-age=10')
     res.end(content)
   })
 });
 router.get('/test.js', function(req, res, next) {
   const cssPath = path.join(__dirname,'../public/javascripts/test.js')
+  console.log('test.js')
   fs.readFile(cssPath,(error,content) => {
-    res.setHeader('Cache-Control','public,max-age=20')
+    res.setHeader('Cache-Control','max-age=10')
     res.end(content)
   })
 });
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+var crypto = require('crypto')
+function getMd5(path){
+  var md5 = crypto.createHash('md5');
+  return new Promise(function(resolve,reject){
+    fs.readFile(path,(error,content) => {
+      resolve(md5.update(content).digest('hex'))
+    })
+  })
+
+}
 
 /* GET users listing. */
-router.use(function timeLog (req, res, next) {
-  console.log('Time: ', new Date())
-  next()
-})
+// router.use(function timeLog (req, res, next) {
+//   console.log('Time: ', new Date())
+//   next()
+// })
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource1');
+  res.sendFile(path.join(__dirname,'../public/html/bj.html'))
+});
+
+router.get('/duibi.css', function(req, res, next) {
+  const cssPath = path.join(__dirname,'../public/stylesheets/duibi.css')
+getMd5(cssPath).then(function(data){
+  console.log('headers',req.headers)
+  if(req.headers['if-none-match'] === data){
+    console.log('304')
+    res.status(304).end('123')
+  }else {
+    fs.readFile(cssPath,(error,content) => {
+      // getMd5(cssPath).then(function(data){
+        res.set('ETag',data)
+        setTimeout(function(){
+          // res.setHeader('Cache-Control','no-cache')
+          res.end(content)
+        },5000)
+      })
+    // })
+  }
+
+})
+
+});
+router.get('/duibi.js', function(req, res, next) {
+  const cssPath = path.join(__dirname,'../public/javascripts/duibi.js')
+  fs.readFile(cssPath,(error,content) => {
+    res.end(content)
+  })
 });
 router.post('/post', function(req, res, next) {
   res.send({key:'json'});
